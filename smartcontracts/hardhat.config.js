eb4a258d78a98302f142323592d00f6ba23cfc8d
dariusjvc <dario.val.cast@gmail.com>
2023-02-26 15:13:57 +0100

    Changes in the sc configurations

--- smartcontracts/hardhat.config.js
+++ smartcontracts/hardhat.config.js
@@ -34,10 +34,7 @@ module.exports = {
     miRed: {
           url: `http://localhost:8545`,
           //url: `https://blockchain-net-${process.env.ENVI}.apps.cluster-01.paymentslabs.com`,
-          accounts: [`0x${process.env.ADMIN_PRIV_KEY}`,
-          `0x${process.env.BANK1_PRIV_KEY}`,
-          `0x${process.env.BANK2_PRIV_KEY}`,
-          `0x${process.env.BANK3_PRIV_KEY}`]
+          accounts: [`0x${process.env.ADMIN_PRIV_KEY}`]
         }
   }
 };
